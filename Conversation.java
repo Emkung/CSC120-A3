9deca368a60c1288158c20d39dc359c004b39aac
Emkung <ekung@smith.edu>
2023-02-13 14:22:34 -0500

    would be really great if the the transcript runs on the end of the loop :/

--- Conversation.java
+++ Conversation.java
@@ -6,53 +6,67 @@ class Conversation {
   public static void main(String[] arguments) {
     // You will start the conversation here.
     Scanner rounds = new Scanner(System.in);
-    System.out.println("How many rounds?");
+    String question = "How many rounds?";
+    System.out.println(question);
     int rounds1 = rounds.nextInt();
     for (int i = 0; i < rounds1; i++){
       Scanner thoughts = new Scanner(System.in);
-      System.out.println("Hi there!  What's on your mind?");
+      String question2 = "Hi there!  What's on your mind?";
+      System.out.println(question2);
       String thoughts1 = thoughts.nextLine();
-      thoughts1 = thoughts1.replace(",", "");
-      thoughts1 = thoughts1.replace(".", "");
-      thoughts1 = thoughts1.replace(";", "");
-      thoughts1 = thoughts1.replace("!", "");
-      thoughts1 = thoughts1.replace("?", "");
-      String [] t_split = thoughts1.split("\\s+");
+      String thoughts3 = thoughts1;
+      thoughts3 = thoughts3.replace(",", "");
+      thoughts3 = thoughts3.replace(".", "");
+      thoughts3 = thoughts3.replace(";", "");
+      thoughts3 = thoughts3.replace("!", "");
+      thoughts3 = thoughts3.replace("?", "");
+      String [] t_split = thoughts3.split("\\s+");
       // System.out.print(Arrays.toString(t_split));
       for (int i2 = 0; i2 < t_split.length; i2++){
       if (t_split[i2].equalsIgnoreCase("I")){
-        thoughts1 = thoughts1.replace("I", "you");
+        thoughts3 = thoughts3.replace("I", "you");
       }if (t_split[0].equalsIgnoreCase("I")){
-        thoughts1 = thoughts1.replace("I", "You");
+        thoughts3 = thoughts3.replace("I", "You");
       }if (t_split[i2].equalsIgnoreCase("you")){
-        thoughts1 = thoughts1.replace("you", "I");
-        thoughts1 = thoughts1.replace("You", "I");
+        thoughts3 = thoughts3.replace("you", "I");
+        thoughts3 = thoughts3.replace("You", "I");
       }if (t_split[i2].equalsIgnoreCase("me")){
-        thoughts1 = thoughts1.replace("me", "you");
-        thoughts1 = thoughts1.replace("Me", "You");
+        thoughts3 = thoughts3.replace("me", "you");
+        thoughts3 = thoughts3.replace("Me", "You");
       }if (t_split[i2].equalsIgnoreCase("am")){
-        thoughts1 = thoughts1.replace("am", "are");
-        thoughts1 = thoughts1.replace("Am", "Are");
+        thoughts3 = thoughts3.replace("am", "are");
+        thoughts3 = thoughts3.replace("Am", "Are");
       }if (t_split[i2].equalsIgnoreCase("my")){
-        thoughts1 = thoughts1.replace("my", "your");
-        thoughts1 = thoughts1.replace("My", "Your");
+        thoughts3 = thoughts3.replace("my", "your");
+        thoughts3 = thoughts3.replace("My", "Your");
       }if (t_split[i2].equalsIgnoreCase("your")){
-        thoughts1 = thoughts1.replace("your", "my");
-        thoughts1 = thoughts1.replace("Your", "My");
+        thoughts3 = thoughts3.replace("your", "my");
+        thoughts3 = thoughts3.replace("Your", "My");
       }
       
-      }thoughts1 = thoughts1 + "?";
+      }thoughts3 = thoughts3 + "?";
     Scanner thoughts_2 = new Scanner(System.in);
-    System.out.println(thoughts1);
+    System.out.println(thoughts3);
     String thoughts2 = thoughts_2.nextLine();
-    System.out.println("See ya!");
+    String gb = "See ya!\n";
+    System.out.println(gb);
     ArrayList<String> convo = new ArrayList<String>();
-    // convo.add(toString(rounds));
-    // convo.add(thoughts1);
-    // convo.add(thoughts2)
+    String rounds_str = Integer.toString(rounds1);
+    convo.add(question);
+    convo.add(rounds_str);
+    convo.add(question2);
+    convo.add(thoughts1);
+    convo.add(thoughts3);
+    convo.add(thoughts2);
+    convo.add(gb);
+
+    String convostr = String.join("\n", convo);
+    System.out.print("Transcript\n" + convostr+"\n");
 
     
     }
+    
+
   }
 
 }
