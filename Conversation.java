549997a5390759cc3736ab358f139edb08514050
Emkung <ekung@smith.edu>
2023-02-14 11:49:23 -0500

    deleted all my codes and restarted the replacement with a for loop and if conditional

--- Conversation.java
+++ Conversation.java
@@ -9,10 +9,11 @@ class Conversation {
     String question = "How many rounds?";
     System.out.println(question);
     int rounds1 = rounds.nextInt();
+    Random r = new Random();
     for (int i = 0; i < rounds1; i++){
       Scanner thoughts = new Scanner(System.in);
       String question2 = "Hi there!  What's on your mind?";
-      System.out.println(question2);
+      System.out.println("\n" + question2);
       String thoughts1 = thoughts.nextLine();
       String thoughts3 = thoughts1;
       thoughts3 = thoughts3.replace(",", "");
@@ -21,53 +22,66 @@ class Conversation {
       thoughts3 = thoughts3.replace("!", "");
       thoughts3 = thoughts3.replace("?", "");
       String [] t_split = thoughts3.split("\\s+");
+      String[] toMirror = {"I", "me", "you", "my", "your", "am", "are"};
+      String[] replacement = {"you", "you","me", "your", "my", "are", "am"};
+      String str = "";
       // System.out.print(Arrays.toString(t_split));
       for (int i2 = 0; i2 < t_split.length; i2++){
-      if (t_split[i2].equalsIgnoreCase("I")){
-        thoughts3 = thoughts3.replace("I", "you");
-      }if (t_split[0].equalsIgnoreCase("I")){
-        thoughts3 = thoughts3.replace("I", "You");
-      }if (t_split[i2].equalsIgnoreCase("you")){
-        thoughts3 = thoughts3.replace("you", "I");
-        thoughts3 = thoughts3.replace("You", "I");
-      }if (t_split[i2].equalsIgnoreCase("me")){
-        thoughts3 = thoughts3.replace("me", "you");
-        thoughts3 = thoughts3.replace("Me", "You");
-      }if (t_split[i2].equalsIgnoreCase("am")){
-        thoughts3 = thoughts3.replace("am", "are");
-        thoughts3 = thoughts3.replace("Am", "Are");
-      }if (t_split[i2].equalsIgnoreCase("my")){
-        thoughts3 = thoughts3.replace("my", "your");
-        thoughts3 = thoughts3.replace("My", "Your");
-      }if (t_split[i2].equalsIgnoreCase("your")){
-        thoughts3 = thoughts3.replace("your", "my");
-        thoughts3 = thoughts3.replace("Your", "My");
+        str = "";
+        for(int h = 0; h < toMirror.length; h++){
+          if(t_split[i2].equalsIgnoreCase(toMirror[h])){
+            str = replacement[h] + " ";
+          }
+        }
+        if (str.equals("")){
+          str += t_split[i2] + " ";
+        }System.out.print(str);
       }
+      // if (t_split[i2].equalsIgnoreCase("I")){
+      //   thoughts3 = thoughts3.replace("I", "you");
+      // }if (t_split[0].equalsIgnoreCase("I")){
+      //   thoughts3 = thoughts3.replace("I", "You");
+      // }if (t_split[i2].equalsIgnoreCase("you")){
+      //   thoughts3 = thoughts3.replace("you", "I");
+      //   thoughts3 = thoughts3.replace("You", "I");
+      // }if (t_split[i2].equalsIgnoreCase("me")){
+      //   thoughts3 = thoughts3.replace("me", "you");
+      //   thoughts3 = thoughts3.replace("Me", "You");
+      // }if (t_split[i2].equalsIgnoreCase("am")){
+      //   thoughts3 = thoughts3.replace("am", "are");
+      //   thoughts3 = thoughts3.replace("Am", "Are");
+      // }if (t_split[i2].equalsIgnoreCase("my")){
+      //   thoughts3 = thoughts3.replace("my", "your");
+      //   thoughts3 = thoughts3.replace("My", "Your");
+      // }if (t_split[i2].equalsIgnoreCase("your")){
+      //   thoughts3 = thoughts3.replace("your", "my");
+      //   thoughts3 = thoughts3.replace("Your", "My");
+      // } String [] ansAr = {};
       
-      }thoughts3 = thoughts3 + "?";
-    Scanner thoughts_2 = new Scanner(System.in);
-    System.out.println(thoughts3);
-    String thoughts2 = thoughts_2.nextLine();
-    String gb = "See ya!\n";
-    System.out.println(gb);
-    ArrayList<String> convo = new ArrayList<String>();
-    String rounds_str = Integer.toString(rounds1);
-    convo.add(question);
-    convo.add(rounds_str);
-    convo.add(question2);
-    convo.add(thoughts1);
-    convo.add(thoughts3);
-    convo.add(thoughts2);
-    convo.add(gb);
-
-    String convostr = String.join("\n", convo);
-    System.out.print("Transcript\n" + convostr+"\n");
+    //   }thoughts3 = thoughts3 + "?";
+    // Scanner thoughts_2 = new Scanner(System.in);
+    // System.out.println(thoughts3);
+    // String thoughts2 = thoughts_2.nextLine();
+    // String gb = "See ya!\n";
+    // System.out.println(gb);
+    // ArrayList<String> convo = new ArrayList<String>();
+    // String rounds_str = Integer.toString(rounds1);
+    // convo.add(question);
+    // convo.add(rounds_str);
+    // convo.add(question2);
+    // convo.add(thoughts1);
+    // convo.add(thoughts3);
+    // convo.add(thoughts2);
+    // convo.add(gb);
+    // String convostr = String.join("\n", convo);
+    // System.out.print("Transcript\n" + convostr + "\n");
 
     
     }
     
 
   }
+
 
 }
 
