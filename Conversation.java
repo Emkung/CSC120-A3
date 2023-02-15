b560c20dfd965fea5e53090b7e92781f5a117b70
Emkung <ekung@smith.edu>
2023-02-15 18:47:47 -0500

    finished the chat box loop trying to work on Transcript

--- Conversation.java
+++ Conversation.java
@@ -1,7 +1,4 @@
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 class Conversation {
   public static void main(String[] arguments) {
     // You will start the conversation here.
@@ -9,79 +6,64 @@ class Conversation {
     String question = "How many rounds?";
     System.out.println(question);
     int rounds1 = rounds.nextInt();
+    // rounds.close();
     Random r = new Random();
+      // Scanner thoughts = new Scanner(System.in);;
+    String question2 = "Hi there!  What's on your mind?";
+    System.out.println("\n" + question2);
     for (int i = 0; i < rounds1; i++){
-      Scanner thoughts = new Scanner(System.in);
-      String question2 = "Hi there!  What's on your mind?";
-      System.out.println("\n" + question2);
-      String thoughts1 = thoughts.nextLine();
+      Scanner input = new Scanner(System.in);
+      String thoughts1 = input.nextLine();
       String thoughts3 = thoughts1;
-      thoughts3 = thoughts3.replace(",", "");
-      thoughts3 = thoughts3.replace(".", "");
-      thoughts3 = thoughts3.replace(";", "");
-      thoughts3 = thoughts3.replace("!", "");
-      thoughts3 = thoughts3.replace("?", "");
+      String[] randomResponse = {"mhm", "that's so interesting!", "I agree", "hmmmm"};
+      // char[] t_chars = thoughts3.toCharArray();
+      // char[] isPunctuation = {',','.', ';', '!', '?'};
+      // for (int j = 0; j < t_chars.length; j++){
+      //   for (int c = 0; c < isPunctuation.length; c++){
+      //     if (t_chars[t_chars.length-1] == isPunctuation[c]){
+      //       thoughts3 = thoughts3.replace(t_chars[t_chars.length-1], '?');
+      //     }else if (t_chars[t_chars.length-1] != isPunctuation[c]){
+      //       t_chars[t_chars.length] = '?';
+      //       String completeThought = new String(t_chars);
+      //       thoughts3 = completeThought;
+      //     }if (t_chars[j] == isPunctuation[c]){
+      //       thoughts3 = thoughts3.replace(t_chars[j], ' ');
+      //     }
+      //   }
       String [] t_split = thoughts3.split("\\s+");
       String[] toMirror = {"I", "me", "you", "my", "your", "am", "are"};
       String[] replacement = {"you", "you","me", "your", "my", "are", "am"};
       String str = "";
-      // System.out.print(Arrays.toString(t_split));
+      int counter = 0;
+      String currentWord;
       for (int i2 = 0; i2 < t_split.length; i2++){
-        str = "";
+        currentWord = "";
         for(int h = 0; h < toMirror.length; h++){
           if(t_split[i2].equalsIgnoreCase(toMirror[h])){
-            str = replacement[h] + " ";
+            counter ++;
+            currentWord = replacement[h];
           }
+          }if (currentWord.equals("")){
+            str += t_split[i2] + " ";
+          }else{ 
+            str += currentWord + " ";
         }
-        if (str.equals("")){
-          str += t_split[i2] + " ";
-        }System.out.print(str);
+      }if (counter > 0){
+        System.out.println(str + "?");
+      }if (counter <= 0){
+        System.out.println(randomResponse[r.nextInt(randomResponse.length)]);
+
       }
-      // if (t_split[i2].equalsIgnoreCase("I")){
-      //   thoughts3 = thoughts3.replace("I", "you");
-      // }if (t_split[0].equalsIgnoreCase("I")){
-      //   thoughts3 = thoughts3.replace("I", "You");
-      // }if (t_split[i2].equalsIgnoreCase("you")){
-      //   thoughts3 = thoughts3.replace("you", "I");
-      //   thoughts3 = thoughts3.replace("You", "I");
-      // }if (t_split[i2].equalsIgnoreCase("me")){
-      //   thoughts3 = thoughts3.replace("me", "you");
-      //   thoughts3 = thoughts3.replace("Me", "You");
-      // }if (t_split[i2].equalsIgnoreCase("am")){
-      //   thoughts3 = thoughts3.replace("am", "are");
-      //   thoughts3 = thoughts3.replace("Am", "Are");
-      // }if (t_split[i2].equalsIgnoreCase("my")){
-      //   thoughts3 = thoughts3.replace("my", "your");
-      //   thoughts3 = thoughts3.replace("My", "Your");
-      // }if (t_split[i2].equalsIgnoreCase("your")){
-      //   thoughts3 = thoughts3.replace("your", "my");
-      //   thoughts3 = thoughts3.replace("Your", "My");
-      // } String [] ansAr = {};
+    }String gb = "See ya!\n";
+  System.out.println(gb);
+  
+
       
-    //   }thoughts3 = thoughts3 + "?";
-    // Scanner thoughts_2 = new Scanner(System.in);
-    // System.out.println(thoughts3);
-    // String thoughts2 = thoughts_2.nextLine();
-    // String gb = "See ya!\n";
-    // System.out.println(gb);
-    // ArrayList<String> convo = new ArrayList<String>();
-    // String rounds_str = Integer.toString(rounds1);
-    // convo.add(question);
-    // convo.add(rounds_str);
-    // convo.add(question2);
-    // convo.add(thoughts1);
-    // convo.add(thoughts3);
-    // convo.add(thoughts2);
-    // convo.add(gb);
-    // String convostr = String.join("\n", convo);
-    // System.out.print("Transcript\n" + convostr + "\n");
-
-    
-    }
-    
-
-  }
-
-
+  } 
 }
+
+  
+
+
+
 
